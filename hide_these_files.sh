1a9cca924bc8b0c0a6ff757692624de931f1818c
Peng Zhang <pzhang@spawar.navy.mil>
2021-03-01 18:35:12 -0800

    added ycm_extra_conf.py

--- hide_these_files.sh
+++ hide_these_files.sh
@@ -2,7 +2,9 @@
 
 for f in *
 do
-	if [ $f != "hide_these_files.sh" ] && [ $f != "README.md" ] ; then
+	if [ $f != "hide_these_files.sh" ] && [ $f != "README.md" ]; then
 		mv $f ~/.$f
 	fi
 done
+
+mv ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
